da3f683d87f4c5b2d72413933d1eddaa62f58bb4
Alvin Cousins <alvin.cousins@googlemail.com>
2021-03-18 19:49:58 +0000

    Working

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("test");
-
 const menuButton = document.querySelector(".menuButton");
 const menu = document.querySelector(".menu");
 
@@ -10,19 +8,114 @@ menuButton.addEventListener("click", () => {
 
 // Image Slider
 
-const images = [
-  "Images/Fili-2016_(113).JPG",
-  "Images/Fili-2016_(144).JPG",
-  "Images/Fili-2016_(173).JPG",
-  "Images/IMG_0219.JPG",
-];
+// Buttons
 
-const btnNext = document.querySelector(".next");
-btnNext.addEventListener("click", () => {
-  console.log("Next");
-});
+const buttons = {
+  btnNext: document.querySelector(".next"),
+  btnPrev: document.querySelector(".prev"),
+  addListeners() {
+    this.btnNext.addEventListener("click", () => {
+      carousel.moveToNextSlide();
+      timerObj.resetTimer();
+    });
+    this.btnPrev.addEventListener("click", () => {
+      carousel.moveToPrevSlide();
+      timerObj.resetTimer();
+    });
+  },
+};
+buttons.addListeners();
 
-const btnPrev = document.querySelector(".prev");
-btnPrev.addEventListener("click", () => {
-  console.log("Prev");
-});
+const carousel = {
+  carouselSlide: document.querySelector(".carousel-slide"),
+  images: document.querySelectorAll(".slides"),
+  imgWidth: 800,
+  currentImg: 1,
+  xCoord: 0,
+  moveToNextSlide() {
+    if (this.currentImg > this.images.length - 2) return;
+    this.carouselSlide.classList.add("moveAnimation");
+    this.currentImg += 1;
+    this.carouselSlide.addEventListener("transitionend", () => {
+      if (this.currentImg === this.images.length - 1) this.setToStart();
+    });
+    this.xCoord = this.imgWidth * this.currentImg;
+    this.carouselSlide.style.transform = `translateX(-${this.xCoord}px)`;
+    radioButtonsObj.updateRadioButtons(this.currentImg);
+  },
+  moveToPrevSlide() {
+    if (this.currentImg < 1) return;
+    this.carouselSlide.classList.add("moveAnimation");
+    this.currentImg -= 1;
+    this.carouselSlide.addEventListener("transitionend", () => {
+      if (this.currentImg === 0) this.setToEnd();
+    });
+    this.xCoord = this.imgWidth * this.currentImg;
+    this.carouselSlide.style.transform = `translateX(-${this.xCoord}px)`;
+    radioButtonsObj.updateRadioButtons(this.currentImg);
+  },
+  setToEnd() {
+    this.currentImg = this.images.length - 2;
+    this.xCoord = this.imgWidth * this.currentImg;
+    this.carouselSlide.classList.remove("moveAnimation");
+    this.carouselSlide.style.transform = `translateX(-${this.xCoord}px)`;
+  },
+  setToStart() {
+    this.currentImg = 1;
+    this.xCoord = this.imgWidth * this.currentImg;
+    this.carouselSlide.classList.remove("moveAnimation");
+    this.carouselSlide.style.transform = `translateX(-${this.xCoord}px)`;
+  },
+  moveToImg(index) {
+    this.currentImg = index + 1;
+    this.xCoord = this.imgWidth * this.currentImg;
+    this.carouselSlide.style.transform = `translateX(-${this.xCoord}px)`;
+    //radioButtonsObj.updateRadioButtons(this.currentImg);
+  },
+};
+// Set initial image to second:
+carousel.carouselSlide.style.transform = `translateX(-${carousel.imgWidth}px)`;
+
+const radioButtonsObj = {
+  radioButtons: Array.from(document.querySelectorAll(".radioBtn")),
+  addListeners() {
+    for (let i = 0; i < this.radioButtons.length; i++) {
+      this.radioButtons[i].addEventListener("click", () => {
+        this.removeActiveBtns();
+        this.radioButtons[i].classList.add("checked");
+        carousel.moveToImg(i);
+        timerObj.resetTimer();
+      });
+    }
+  },
+  removeActiveBtns() {
+    for (button of this.radioButtons) {
+      button.classList.remove("checked");
+    }
+  },
+  updateRadioButtons(index) {
+    let i = index - 1;
+    if (i < 0) i = this.radioButtons.length - 1;
+    if (i > this.radioButtons.length - 1) i = 0;
+    this.removeActiveBtns();
+    this.radioButtons[i].classList.add("checked");
+  },
+};
+radioButtonsObj.addListeners();
+
+const timerObj = {
+  waitTime: 5000,
+  clock: 0,
+  timer() {
+    let counter = 0;
+    this.clock = setInterval(() => {
+      counter++;
+      carousel.moveToNextSlide();
+    }, this.waitTime);
+  },
+  resetTimer() {
+    clearInterval(this.clock);
+    this.timer();
+  },
+};
+timerObj.timer();
